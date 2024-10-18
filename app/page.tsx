aa148c160cfabde2de239f806eef2cc4cd1108a7
Gjermund Persson Myrvang <gjermupm@uio.no>
2024-10-18 15:29:07 +0200

    change to white background

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Content from "@/app/markdown/Content.mdx";
 export default function Home() {
   return (
-    <main className="flex justify-center items-center my-5">
+    <main className="flex justify-center items-center my-5 bg-white">
       <div className="relative flex flex-col max-w-7xl w-full justify-center items-center sm:p-5">
         <Content />
       </div>
