7716b83e859224e049103d40f9b21a4e527386e7
Gjermund Persson Myrvang <gjermupm@uio.no>
2024-10-18 15:45:59 +0200

    add some padding

--- app/contents/terms/page.tsx
+++ app/contents/terms/page.tsx
@@ -3,7 +3,7 @@ import TermsConditions from "@/app/markdown/TermsConditions.mdx";
 export default function Home() {
     return (
       <main className="flex justify-center items-center bg-white">
-        <div className="mt-5 relative flex flex-col max-w-7xl w-full justify-center items-center sm:px-5">
+        <div className="mt-5 relative flex flex-col max-w-7xl w-full justify-center items-center px-5">
           <TermsConditions />
         </div>
       </main>
