b4a1e9e6b83d2c39da023a38977d03d930b689be
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-03-30 21:43:49 +0200

    add page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Content from "@/app/markdown/Content.mdx";
 export default function Home() {
   return (
     <main className="flex justify-center items-center bg-white min-h-screen">
-      <div className="mt-5 relative flex flex-col max-w-7xl w-full justify-center items-center sm:p-5">
+      <div className="mt-5 relative flex flex-col max-w-7xl w-full justify-center items-center p-5">
         <Content />
       </div>
     </main>
